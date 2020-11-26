43fcb8f5a203974b9e17cd212df51c6b2b5899f0
Eduardo Gonzalez <eduardsalvadorgonzalez@gmail.com>
2020-11-25 18:31:30 -0600

    lista del menu!

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Componente {
+  icon: string,
+  name: string,
+  redirectTo: string
+}
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
-
+  public components: Componente[] = [
+    {
+      icon:'game-controller-outline',
+      name:'Action sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon:'skull-outline',
+      name:'Alert',
+      redirectTo: '/alert' 
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
